f2e66c9d9b7b4c49cf9cc0773951b0e9c061f1d4
wiederm <marcus.wieder@univie.ac.at>
2021-01-25 12:43:46 +0100

    more files updated

--- toluene/waterbox/openmm/r.sh
+++
@@ -1,6 +0,0 @@
-set init = step3_charmm2omm
-set cnt = 1
-set istep = step4_equilibration
-set input = step5_production
-set istep = step5_${cnt}
-python -u openmm_run.py -i ${input}.inp -t toppar.str -p ${init}.psf -c ${init}.crd  -odcd ${istep}.dcd 
